993afc980901d4251c67ddc0630f8afc3e771995
晋晨曦 <Jin0714@outlook.com>
2024-04-16 09:46:45 +0800

    feat(doucument): add code of document

---
+++ src/hust/cs/javacourse/search/index/impl/Document.java
@@ -0,0 +1,123 @@
+package hust.cs.javacourse.search.index.impl;
+
+import hust.cs.javacourse.search.index.AbstractDocument;
+import hust.cs.javacourse.search.index.AbstractTermTuple;
+
+import java.util.List;
+
+/**
+ * Document类是AbstractDocument的具体实现。
+ * 它包含了文档的ID，文档的路径，以及文档包含的词元三元组列表。
+ * 提供了获取和设置这些属性的方法，以及添加和获取三元组的方法。
+ *
+ * @author 晋晨曦
+ */
+public class Document extends AbstractDocument {
+    /**
+     * 获取文档的ID。
+     *
+     * @return 文档的ID
+     */
+    @Override
+    public int getDocId() {
+        return docId;
+    }
+
+    /**
+     * 设置文档的ID。
+     *
+     * @param docId 要设置的文档ID
+     */
+    @Override
+    public void setDocId(int docId) {
+        this.docId = docId;
+    }
+
+    /**
+     * 获取文档的路径。
+     *
+     * @return 文档的路径
+     */
+    @Override
+    public String getDocPath() {
+        return docPath;
+    }
+
+    /**
+     * 设置文档的路径。
+     *
+     * @param docPath 要设置的文档路径
+     */
+    @Override
+    public void setDocPath(String docPath) {
+        this.docPath = docPath;
+    }
+
+    /**
+     * 获取文档包含的词元三元组列表。
+     *
+     * @return 文档包含的词元三元组列表
+     */
+    @Override
+    public List<AbstractTermTuple> getTuples() {
+        return tuples;
+    }
+
+    /**
+     * 向文档中添加一个词元三元组，如果该三元组已存在，则不添加。
+     *
+     * @param tuple 要添加的词元三元组
+     */
+    @Override
+    public void addTuple(AbstractTermTuple tuple) {
+        if (!tuples.contains(tuple)) {
+            tuples.add(tuple);
+        }
+    }
+
+    /**
+     * 判断文档是否包含指定的词元三元组。
+     *
+     * @param tuple 指定的词元三元组
+     * @return 如果文档包含指定的词元三元组，返回true，否则返回false
+     */
+    @Override
+    public boolean contains(AbstractTermTuple tuple) {
+        return tuples.contains(tuple);
+    }
+
+    /**
+     * 获取指定下标位置的词元三元组。
+     *
+     * @param index 指定的下标位置
+     * @return 指定下标位置的词元三元组
+     */
+    @Override
+    public AbstractTermTuple getTuple(int index) {
+        return tuples.get(index);
+    }
+
+    /**
+     * 获取文档包含的词元三元组的数量。
+     *
+     * @return 文档包含的词元三元组的数量
+     */
+    @Override
+    public int getTupleSize() {
+        return tuples.size();
+    }
+
+    /**
+     * 返回文档的字符串表示形式。
+     *
+     * @return 文档的字符串表示形式
+     */
+    @Override
+    public String toString() {
+        return "Document{" +
+                "docId=" + docId +
+                ", docPath='" + docPath + '\'' +
+                ", tuples=" + tuples +
+                '}';
+    }
+}
---
+++ src/hust/cs/javacourse/search/index/impl/Index.java
@@ -0,0 +1,4 @@
+package hust.cs.javacourse.search.index.impl;/**
+ * @author 晋晨曦
+ */public class Index {
+}
